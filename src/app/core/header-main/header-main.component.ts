375414a9803663238d15830e8f9d6edfbee60df9
Anghamhu <anghamhussein777@gmail.com>
2025-08-01 18:56:04 +0300

    ediet

--- src/app/core/header-main/header-main.component.ts
+++ src/app/core/header-main/header-main.component.ts
@@ -10,9 +10,9 @@ export class HeaderMainComponent implements OnInit {
 
   changeLanguage(lang: string): void {
     this.currentLang = lang;
-    // هون لاحقًا فيك تستخدمي خدمة ترجمة (ngx-translate)
+    
     console.log('Language switched to:', lang);
-    // مثال: window.location.href = '/en' أو reload app
+    
   }
   constructor() { }
 
